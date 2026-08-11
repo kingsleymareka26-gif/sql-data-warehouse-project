0e80de83a08e79609b830c87b6dc0617401c2d5a
kingsleymareka26-gif <kingsleymareka26@gmail.com>
2026-08-11 16:11:44 +0200

    Update int_database.sql

--- scripts/int_database.sql
+++ scripts/int_database.sql
@@ -1,1 +1,15 @@
 
+-- Create Database 'DataWarehouse'
+
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
